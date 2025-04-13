cad909e02a2d88ce5f64daf5e310795b93f96a39
João Paulo Bertagia <162495527+jbertagia@users.noreply.github.com>
2025-04-13 11:21:36 -0300

    Update header.php
    atualização incluindo modificações na apresentação dos botões.
    logout, meus jogos e cadastro > so aparece quando logado
    login > aparece quando não logado
    acrescentada msg de que o usuario delogou

--- catalogoJogosFavoritos/header.php
+++ catalogoJogosFavoritos/header.php
@@ -7,12 +7,18 @@ $usuario = $_SESSION['usuario'] ?? null;
 ?>
 
 <header>
-<div class="menu">
-    <div class="nav-links">
-        <a href="index.php">Início</a>
-        <a href="meus_jogos.php">Meus Jogos</a>
-        <a href="cadastro.php">Cadastrar Novo Jogo</a>
+    <div class="menu">
+        <div class="nav-links">
+            <a href="index.php">Início</a>
+            <?php if ($usuario): ?>
+                <a href="meus_jogos.php">Meus Jogos</a>
+                <a href="cadastro.php">Cadastrar Novo Jogo</a>
+            <?php endif; ?>
+        </div>
+        <?php if ($usuario): ?>
+            <a href="logout.php" class="logout">Logout</a>
+        <?php else: ?>
+            <a href="login.php?origem=index.php" class="logout">Login</a>
+        <?php endif; ?>
     </div>
-    <a href="logout.php" class="logout">Logout</a>
-</div>
 </header>
